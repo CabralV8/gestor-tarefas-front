7ce0a1f8371443c209199c50008dddfdc6e8e83e
Valberton <valberton77@gmail.com>
2025-09-29 11:46:07 -0300

    create app top menu

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from './shared/components/global/top-menu/top-menu.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TopMenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'agendador-tarefas';
+}
